0d188e528fc171f95c7fac74891ac40ff510679d
Aman Sharma <aman8696.56@gmail.com>
2022-01-13 04:45:06 +0530

    feat: Add approve function and transferFrom function, and allowance mapping property

--- test/Token.test.js
+++ test/Token.test.js
@@ -3,7 +3,7 @@ require('chai').use(require('chai-as-promised')).should()
 
 const Token = artifacts.require("./Token")
 
-contract('Token', ([deployer, receiver]) => {
+contract('Token', ([deployer, receiver, exchange]) => {
     const name = 'NerdX Token';
     const symbol = 'NEX';
     const decimals = '18';
@@ -83,6 +83,89 @@ contract('Token', ([deployer, receiver]) => {
 
             it('rejects invalid recipients', async () => {
                 await token.transfer(0x0, amount, { from: deployer }).should.be.rejected;
+            })
+        })
+    })
+
+    describe("approving tokens", () => {
+        let result
+        let amount
+
+        beforeEach(async () => {
+            amount = tokens(100)
+            result = await token.approve(exchange, amount, { from: deployer })
+        })
+
+        describe("success", () => {
+            it("allocates an allowance for delegated token spending on an exchange", async () => {
+                const allowance = await token.allowance(deployer, exchange)
+                allowance.toString().should.equal(amount.toString())
+            })
+
+            it("emits an Approval event", async () => {
+                const log = result.logs[0];
+                log.event.should.equal("Approval");
+                const event = log.args;
+                event.owner.toString().should.equal(deployer, "owner is correct");
+                event.spender.should.equal(exchange, "spender is correct");
+                event.value.toString().should.equal(amount.toString(), "value is correct");
+            })
+        })
+
+        describe("failure", () => {
+            it('rejects invalid recipients', async () => {
+                await token.transfer(0x0, amount, { from: deployer }).should.be.rejected;
+            })
+        })
+    })
+
+    describe("delegated token transfer", () => {
+        let amount, result;
+
+        beforeEach(async () => {
+            amount = tokens(100);
+            // Approve
+            result = await token.approve(exchange, amount, { from: deployer }) // { from: deployer } is a metadata that we provide for the function to derive msg.sender from it. It is a web3.js concept.
+        })
+
+        describe("success", async () => {
+            beforeEach(async () => {
+                // Transfer
+                result = await token.transferFrom(deployer, receiver, amount, { from: exchange }) // { from: deployer } is a metadata that we provide for the function to derive msg.sender from it. It is a web3.js concept.
+            })
+
+            it('transfers token balances', async () => {
+                let balanceOf;
+                balanceOf = await token.balanceOf(deployer);
+                balanceOf.toString().should.equal(tokens(999900).toString())
+                balanceOf = await token.balanceOf(receiver);
+                balanceOf.toString().should.equal(tokens(100).toString())
+            })
+
+            it("resets the allowance", async () => {
+                const allowance = await token.allowance(deployer, exchange)
+                allowance.toString().should.equal('0')
+            })
+
+            it("emits a Transfer event", async () => {
+                const log = result.logs[0];
+                log.event.should.equal("Transfer");
+                const event = log.args;
+                event.from.toString().should.equal(deployer, "from is correct");
+                event.to.should.equal(receiver, "to is correct");
+                event.value.toString().should.equal(amount.toString(), "value is correct");
+            })
+        })
+
+        describe("failure", async () => {
+            it('rejects insufficient amounts', async () => {
+                let invalidAmount
+                invalidAmount = tokens(1000000000) // greater than the totalSupply and allowance
+                await token.transferFrom(deployer, receiver, invalidAmount, { from: exchange }).should.be.rejectedWith(EVM_REVERT)
+            })
+
+            it('rejects invalid recipients', async () => {
+                await token.transfer(deployer, 0x0, amount, { from: exchange }).should.be.rejected;
             })
         })
     })
