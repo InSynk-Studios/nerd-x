cbf90b239efbc763afcd39a80c243eb1113f0288
Aman Sharma <aman8696.56@gmail.com>
2022-01-14 05:44:07 +0530

    feat: Withdraw tokens and ethers functions, Check balance function. WIP: Exchange smart contract

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -8,10 +8,10 @@ import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
     [X] Set the fee percent when we deploy the smart contract
     [X] Set the fee account
     [X] Deposit Ether
-    [ ] Withdraw Ether
+    [X] Withdraw Ether
     [X] Deposit tokens
-    [ ] Withdraw tokens
-    [ ] Check balances
+    [X] Withdraw tokens
+    [X] Check balances
     [ ] Make order 
     [ ] Cancel order
     [ ] Fill order
@@ -27,6 +27,12 @@ contract Exchange {
     mapping(address => mapping(address => uint256)) public tokens; // First key is token address, second is user address (who is depositing token) and value is total no. of tokens deposited by user (basically his balance on the exchange).
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(
+        address token,
+        address user,
+        uint256 amount,
+        uint256 balance
+    );
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -34,8 +40,8 @@ contract Exchange {
     }
 
     fallback() external {
-      // Fallback function: reverts if Ether is sent to this smart contract address by mistake. 
-      revert();
+        // Fallback function: reverts if Ether is sent to this smart contract address by mistake.
+        revert();
     }
 
     function depositEther() public payable {
@@ -43,10 +49,33 @@ contract Exchange {
         emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
     }
 
+    function withdrawEther(uint256 _amount) public payable {
+        require(tokens[ETHER][msg.sender] >= _amount);
+        tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].sub(_amount);
+        payable(msg.sender).transfer(_amount);
+        emit Withdraw(ETHER, msg.sender, _amount, tokens[ETHER][msg.sender]);
+    }
+
     function depositToken(address _token, uint256 _amount) public {
         require(_token != ETHER);
         require(Token(_token).transferFrom(msg.sender, address(this), _amount));
         tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
         emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function withdrawToken(address _token, uint256 _amount) public {
+        require(_token != ETHER);
+        require(tokens[_token][msg.sender] >= _amount);
+        tokens[_token][msg.sender] = tokens[_token][msg.sender].sub(_amount);
+        require(Token(_token).transfer(msg.sender, _amount));
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function balanceOf(address _token, address _user)
+        public
+        view
+        returns (uint256)
+    {
+        return tokens[_token][_user];
     }
 }
