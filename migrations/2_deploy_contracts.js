bbc3cf98d4a84208160da36abcb8bb2da866b365
Aman Sharma <aman8696.56@gmail.com>
2022-01-16 20:07:37 +0530

    feat: Add comments

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,10 +4,10 @@ const Exchange = artifacts.require("Exchange");
 module.exports = async function (deployer) {
   const accounts = await web3.eth.getAccounts(); // An array of all arrays provided by ganache
   
-  await deployer.deploy(Token);
+  await deployer.deploy(Token); // Deploy Token on blockchain
 
   const feeAccount = accounts[0];
   const feePercent = 10;
 
-  await deployer.deploy(Exchange, feeAccount, feePercent);
+  await deployer.deploy(Exchange, feeAccount, feePercent); // Deploy Exchange on blockchain
 };
