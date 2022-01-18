cc30c1848efb50d038a76863cda3ae7175854ba6
Aman Sharma <aman8696.56@gmail.com>
2022-01-18 16:33:56 +0530

    feat: Change navbar logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
 import { Component } from "react";
 import { connect } from "react-redux";
+import logo from '../assets/nerdx-logo-md.png';
 import { accountSelector } from "../store/selectors";
 
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-        <a className="navbar-brand" href="/#">Nerd Exchange (NerdX)</a>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3 position-relative">
+        <a className="navbar-brand" href="/#">
+          <span style={{ height: '35px', display: 'block' }}></span>
+          <img
+            src={logo}
+            className="App-logo position-absolute"
+            alt="logo"
+            style={{
+              height: '50px',
+              top: '4px'
+            }} />
+        </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
