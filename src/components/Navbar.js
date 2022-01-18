1365827f884480c74cbad029077a50c23f10c7ff
Aman Sharma <aman8696.56@gmail.com>
2022-01-18 21:18:19 +0530

    feat: Add Price Chart component, and some css changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,9 +18,9 @@ class Navbar extends Component {
               top: '5px'
             }} />
         </a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
-        </button>
+        </button> */}
         <ul className="navbar-nav ms-auto">
           <li className="nav-item">
             <a
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -5,7 +5,7 @@
  * and you can do any kind of filtering/modification of that data.
  */
 
-import { get, groupBy, reject } from 'lodash'
+import { get, groupBy, maxBy, minBy, reject } from 'lodash'
 import moment from 'moment'
 import { createSelector } from 'reselect'
 import { ETHER_ADDRESS, tokens, ether, GREEN, RED } from '../helpers'
@@ -270,4 +270,63 @@ const decorateMyOpenOrder = (order, account) => {
     orderType,
     orderTypeClass: (orderType === 'buy' ? GREEN : RED)
   })
+}
+
+export const priceChartLoadedSelector = createSelector(filledOrdersLoaded, loaded => loaded)
+
+export const priceChartSelector = createSelector(
+  filledOrders,
+  (orders) => {
+    // Sort by ascending date to compare history
+    orders = orders.sort((a, b) => a.timestamp - b.timestamp)
+    // Decorate ordes - add display attributes
+    orders = orders.map((o) => decorateOrder(o))
+    // Get last two orders final price & price change
+    let secondLastOrder, lastOrder
+    [secondLastOrder, lastOrder] = orders.slice(orders.length - 2, orders.length)
+    // Get last order price
+    const lastPrice = get(lastOrder, 'tokenPrice', 0)
+    // Get second last order price
+    const secondLastPrice = get(secondLastOrder, 'tokenPrice', 0)
+
+    return ({
+      lastPrice,
+      lastPriceChange: (lastPrice >= secondLastPrice ? '+' : '-'),
+      series: [{
+        data: buildGraphData(orders)
+      }]
+    })
+  }
+)
+
+/**
+ * Group the orders into a format that is grouped by hour, 
+ * so each candle will be an hour. 
+ */
+const buildGraphData = (orders) => {
+  // Group the orders by hour for the graph
+  orders = groupBy(orders, (o) => moment.unix(o.timestamp).startOf('hour'))
+  // Get each hour where data exists
+  const hours = Object.keys(orders)
+  // Build the graph series
+  const graphData = hours.map((hour) => {
+    // Fetch all the orders from current hour
+    const group = orders[hour]
+    // Calculate price values - open, high, low, close
+    const open = group[0] // first order
+    const high = maxBy(group, 'tokenPrice') // high price
+    const low = minBy(group, 'tokenPrice') // low price
+    const close = group[group.length - 1] // last order
+
+    return ({
+      x: new Date(hour), // This will be the hour where data exists
+      y: [
+        open.tokenPrice,
+        high.tokenPrice,
+        low.tokenPrice,
+        close.tokenPrice
+      ] // This will be all the Open, High, Low, Close values
+    })
+  })
+  return graphData
 }
